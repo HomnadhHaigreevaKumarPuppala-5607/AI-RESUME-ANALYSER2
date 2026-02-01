f89348cb339a5d3d4f11832a4345e9472a26e1da
HomnadhHaigreevaKumarPuppala-5607 <puppalahaigreevakumar@gmail.com>
2026-02-01 20:55:02 +0530

    analyze_routes.py

--- backend/routes/analyze_routes.py
+++ backend/routes/analyze_routes.py
@@ -3,7 +3,7 @@ from utils.resume_analyzer import analyze_resume
 
 analyze_bp = Blueprint("analyze", __name__)
 
-@analyze_bp.route("/", methods=["POST"])
+@analyze_bp.route("/analyze", methods=["POST"])
 def analyze():
     data = request.get_json()
     resume = data.get("resume", "")
@@ -11,4 +11,5 @@ def analyze():
 
     result = analyze_resume(resume, job)
     return jsonify(result)
+
 
